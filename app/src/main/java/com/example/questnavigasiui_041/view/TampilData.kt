83198f23d9bd382064f1e4b308b18e3a3c8b78b9
lilalr <gurennut@gmail.com>
2025-11-07 16:22:22 +0700

    menambah scaffold utk struktur layout

--- app/src/main/java/com/example/questnavigasiui_041/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_041/view/TampilData.kt
@@ -36,3 +36,11 @@ fun TampilData(
         Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Lainnya"),
         Pair(first = stringResource(id = R.string.alamat), second = "Yogyakarta"),
     )
+    Scaffold( modifier = Modifier,
+        topBar={
+            TopAppBar(
+                title = {Text(text = stringResource(id = R.string.tampil), color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id = R.color.teal_700))
+            )
+        }
+    ){

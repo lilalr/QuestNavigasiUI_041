c4565510559a6dd0b4bdee0a43043ae962bb254a
lilalr <gurennut@gmail.com>
2025-11-07 15:36:37 +0700

    scaffold utk membuat struktur layout dasar

--- app/src/main/java/com/example/questnavigasiui_041/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_041/view/Formulir.kt
@@ -34,3 +34,11 @@ fun FormIsian(
     jenisK:List<String> = listOf("Laki-laki","Perempuan"),
     OnSubmitBtnClick : () -> Unit
 ){
+    Scaffold (modifier=Modifier,
+        topBar={
+            TopAppBar(
+                title = {Text(text= stringResource(id=R.string.home), color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700))
+            )   }
+    ){
